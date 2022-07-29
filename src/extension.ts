7269d5b8caec508d9ca840fede947ea3611a4546
James Gentes <jgentes@cloudflare.com>
2022-07-29 10:40:18 -0700

    cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -1,144 +1,135 @@
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(
-		vscode.commands.registerCommand('wc.loadWebContainer', () => {
-			WebContainerPanel.createOrShow(context.extensionUri);
-		})
-	);
+  context.subscriptions.push(
+    vscode.commands.registerCommand('wc.loadWebContainer', () => {
+      WebContainerPanel.createOrShow(context.extensionUri)
+    })
+  )
 
-    context.subscriptions.push(
-		vscode.commands.registerCommand('wc.bootWebContainer', () => {
-            console.log('bootWebContainer');
-            WebContainerPanel.currentPanel && WebContainerPanel.currentPanel.sendMessage('boot');
-		})
-	);
-/*
-	if (vscode.window.registerWebviewPanelSerializer) {
-		// Make sure we register a serializer in activation event
-		vscode.window.registerWebviewPanelSerializer(WebContainerPanel.viewType, {
-			async deserializeWebviewPanel(webviewPanel: vscode.WebviewPanel, state: any) {
-				console.log(`Got state: ${state}`);
-				// Reset the webview options so we use latest uri for `localResourceRoots`.
-				webviewPanel.webview.options = getWebviewOptions(context.extensionUri);
-				WebContainerPanel.revive(webviewPanel, context.extensionUri);
-			}
-		});
-	}
-    */
+  context.subscriptions.push(
+    vscode.commands.registerCommand('wc.bootWebContainer', () => {
+      console.log('bootWebContainer')
+      WebContainerPanel.currentPanel &&
+        WebContainerPanel.currentPanel.sendMessage('boot')
+    })
+  )
 }
 
 function getWebviewOptions(extensionUri: vscode.Uri): vscode.WebviewOptions {
-	return {
-		// Enable javascript in the webview
-		enableScripts: true,
+  return {
+    // Enable javascript in the webview
+    enableScripts: true,
 
-		// And restrict the webview to only loading content from the `dist` directory.
-		localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'dist')]
-	};
+    // And restrict the webview to only loading content from the `dist` directory.
+    localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'dist')],
+  }
 }
 
 /**
  * Manages WebContainer webview panels
  */
 class WebContainerPanel {
-	/**
-	 * Track the currently panel. Only allow a single panel to exist at a time.
-	 */
-	public static currentPanel: WebContainerPanel | undefined;
+  /**
+   * Track the currently panel. Only allow a single panel to exist at a time.
+   */
+  public static currentPanel: WebContainerPanel | undefined
 
-	public static readonly viewType = 'WebContainer';
+  public static readonly viewType = 'WebContainer'
 
-	private readonly _panel: vscode.WebviewPanel;
-	private readonly _extensionUri: vscode.Uri;
-	private _disposables: vscode.Disposable[] = [];
+  private readonly _panel: vscode.WebviewPanel
+  private readonly _extensionUri: vscode.Uri
+  private _disposables: vscode.Disposable[] = []
 
-	public static createOrShow(extensionUri: vscode.Uri) {
-        
-		const column = vscode.window.activeTextEditor
-			? vscode.window.activeTextEditor.viewColumn
-			: undefined;
-            
-		// If we already have a panel, show it.
-		if (WebContainerPanel.currentPanel) {
-			WebContainerPanel.currentPanel._panel.reveal(column);
-			return;
-		}
-        
-		// Otherwise, create a new panel.
-		const panel = vscode.window.createWebviewPanel(
-			WebContainerPanel.viewType,
-			'StackBlitz',
-			column || vscode.ViewColumn.One,
-			getWebviewOptions(extensionUri),
-		);
-     
-		WebContainerPanel.currentPanel = new WebContainerPanel(panel, extensionUri);
-	}
-/*
+  public static createOrShow(extensionUri: vscode.Uri) {
+    const column = vscode.window.activeTextEditor
+      ? vscode.window.activeTextEditor.viewColumn
+      : undefined
+
+    // If we already have a panel, show it.
+    if (WebContainerPanel.currentPanel) {
+      WebContainerPanel.currentPanel._panel.reveal(column)
+      return
+    }
+
+    // Otherwise, create a new panel.
+    const panel = vscode.window.createWebviewPanel(
+      WebContainerPanel.viewType,
+      'StackBlitz',
+      column || vscode.ViewColumn.One,
+      getWebviewOptions(extensionUri)
+    )
+
+    WebContainerPanel.currentPanel = new WebContainerPanel(panel, extensionUri)
+  }
+  /*
 	public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
 		WebContainerPanel.currentPanel = new WebContainerPanel(panel, extensionUri);
 	}
 */
-    public sendMessage(message: string) {
-		// Send a message to the webview webview.
-		// You can send any JSON serializable data.
-		this._panel.webview.postMessage({message});
-	}
+  public sendMessage(message: string) {
+    // Send a message to the webview webview.
+    // You can send any JSON serializable data.
+    this._panel.webview.postMessage({ message })
+  }
 
-	private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
-        console.log('new panel')
-		this._panel = panel;
-		this._extensionUri = extensionUri;
+  private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+    console.log('new panel')
+    this._panel = panel
+    this._extensionUri = extensionUri
 
-		// Set the webview's initial html content
-		const webview = this._panel.webview;
+    // Set the webview's initial html content
+    const webview = this._panel.webview
 
-		// Listen for when the panel is disposed
-		// This happens when the user closes the panel or when the panel is closed programmatically
-		this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+    // Listen for when the panel is disposed
+    // This happens when the user closes the panel or when the panel is closed programmatically
+    this._panel.onDidDispose(() => this.dispose(), null, this._disposables)
 
-		// Handle messages from the webview
-		this._panel.webview.onDidReceiveMessage(
-			message => {
-				switch (message.command) {
-					case 'alert':
-						vscode.window.showErrorMessage(message.text);
-						return;
-				}
-			},
-			null,
-			this._disposables
-		);
-        
-		this._panel.webview.html = this._getHtmlForWebview(webview);
-	}
+    // Handle messages from the webview
+    this._panel.webview.onDidReceiveMessage(
+      message => {
+        switch (message.command) {
+          case 'alert':
+            vscode.window.showErrorMessage(message.text)
+            return
+        }
+      },
+      null,
+      this._disposables
+    )
 
-	public dispose() {
-		WebContainerPanel.currentPanel = undefined;
+    this._panel.webview.html = this._getHtmlForWebview(webview)
+  }
 
-		// Clean up our resources
-		this._panel.dispose();
+  public dispose() {
+    WebContainerPanel.currentPanel = undefined
 
-		while (this._disposables.length) {
-			const x = this._disposables.pop();
-			if (x) {
-				x.dispose();
-			}
-		}
-	}
+    // Clean up our resources
+    this._panel.dispose()
 
-	private _getHtmlForWebview(webview: vscode.Webview) {
-		// Local path to script to run in the webview
-		const mainScriptPath = vscode.Uri.joinPath(this._extensionUri, 'dist', 'main.js');        
+    while (this._disposables.length) {
+      const x = this._disposables.pop()
+      if (x) {
+        x.dispose()
+      }
+    }
+  }
 
-		// And the uri we use to load this script in the webview
-		const mainScriptUri = webview.asWebviewUri(mainScriptPath);
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    // Local path to script to run in the webview
+    const mainScriptPath = vscode.Uri.joinPath(
+      this._extensionUri,
+      'dist',
+      'main.js'
+    )
 
-		return `<!DOCTYPE html>
+    // And the uri we use to load this script in the webview
+    const mainScriptUri = webview.asWebviewUri(mainScriptPath)
+
+    return `<!DOCTYPE html>
                     <body>
 				        <script src="${mainScriptUri}"></script>
 			        </body>
-                </html>`;
-	}
+                </html>`
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,40 +1,32 @@
 // This script will be run within the webview itself
 // It cannot access the main VS Code APIs directly.
-import { load } from '@webcontainer/api';
+import { load } from '@webcontainer/api'
 
+;(async function () {
+  let WebContainer
+  console.log('crossOriginIsolated', crossOriginIsolated)
 
-(async function () {
-    let WebContainer
-    const vscode = acquireVsCodeApi();
+  // Handle messages sent from the extension to the webview
+  window.addEventListener('message', async event => {
+    const { message } = event.data // The json data that the extension sent
+    console.log('webview received message:', message)
+    if (message == 'boot') {
+      // only a single instance of WebContainer can be created
+      try {
+        console.log('booting container')
+        await WebContainer.boot()
+        console.log('we have webcontainer')
+      } catch (e) {
+        console.error('webcontainer error:', e)
+      }
+    }
+  })
 
-    const oldState = vscode.getState();
+  window.addEventListener('error', event => {
+    console.log('webview received error:', event)
+  })
 
-    //console.log('Initial state', oldState);
-    console.log('crossOriginIsolated', crossOriginIsolated)
+  WebContainer = await load()
 
-    // Handle messages sent from the extension to the webview
-    window.addEventListener('message', async event => {
-        const {message} = event.data; // The json data that the extension sent
-        console.log('webview received message:', message)
-        if (message == 'boot') {
-            // only a single instance of WebContainer can be created
-            try {
-                console.log('booting container')
-                await WebContainer.boot();
-                console.log('we have webcontainer')
-                } catch(e) {
-                    console.error('webcontainer error:', e);
-                }
-        }
-    });
-
-    window.addEventListener('error', event => {
-        console.log('webview received error:', event)
-    });
-
-    WebContainer = await load();
-    
-    console.log('webcontainer loaded!')
-    
-    
-}());
+  console.log('webcontainer loaded!')
+})()
